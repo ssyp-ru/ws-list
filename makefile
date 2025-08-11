2008842aee9dc1cb601b8b8778d0dc38e07a901f
myxo <klimov.ns@mail.ru>
2025-08-11 21:27:11 +0300

    fix some issues

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 add_submodules_from_readme:
-	python3 scripts/add_submodules.py
+	python3 scripts/add_submodules.py --exclude ssyp18-ws07
 
 download_project_backup:
 	git submodule update --init --remote
--- scripts/add_submodules.py
+++ scripts/add_submodules.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+import argparse
 import os
 import subprocess
 
-def parse_repo_list():
+
+def parse_repo_list(exclude_list):
     ret = []
     search_line = 'https://github.com/ssyp-ru/'
     with open('README.md', 'r') as file:
@@ -15,9 +17,12 @@ def parse_repo_list():
             closed_bracket = line.find(')', index)
             url = line[index:closed_bracket]
             repo_name = line[index+len(search_line):closed_bracket]
+            if repo_name in exclude_list:
+                print('Ignoring %s'%repo_name)
+                continue
+
             sp = repo_name.split('-', 1)
             year, ws = sp[0], sp[1]
-
             ret.append((url, year, ws))
     return ret
 
@@ -28,12 +33,17 @@ def add_submodule_if_need(repo_list):
         if not os.path.exists(folder_path):
             print('add git submodule for url=%s path=%s'%(url, folder_path))
             result = subprocess.run(['git', 'submodule', 'add', url, folder_path], capture_output=True, text=True)
+            print(f"Command executed: {' '.join(result.args)}")
             print("Stdout:", result.stdout)
             print("Stderr:", result.stderr)
             print("Return code:", result.returncode)
             print('')
 
 if __name__ == '__main__':
-    repo_list = parse_repo_list()
+    parser = argparse.ArgumentParser(description='Little helper for add SSYP project as submodule (for quick backup)')
+    parser.add_argument('--exclude', help='project to exclude', action='append')
+    args = parser.parse_args()
+
+    repo_list = parse_repo_list(args.exclude)
     add_submodule_if_need(repo_list)
     print('done.')
